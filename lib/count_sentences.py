63bcf56a216ae38217740641fa462bb81109017f
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-23 13:19:47 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# import string
+import re
 
 
 class MyString:
@@ -19,7 +19,7 @@ class MyString:
         if isinstance(value, str):
             self._value = value
         else:
-            print("The value must be a string.")
+            print("The value must be a string.")  # Raise ValueError instead of printing
 
     def is_sentence(self):
         trimmed = self._value.strip()  # Remove leading and trailing whitespace
@@ -46,13 +46,22 @@ class MyString:
         if the value ends with
         an exclamation mark and False if it does not.
         """
+        trimmed = self._value.strip()  # Remove leading and trailing whitespace
+        if trimmed.endswith("!"):
+            return True
+        else:
+            return False
 
     def count_sentences(self):
-        pass
+        sentences = re.split(r"[.!?]", self._value)
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        num_of_sentences = len(sentences)
+        return num_of_sentences
 
 
-# some_sentence = MyString(456)
-# some_sentence.value = 456
-some_sentence = MyString("MyString has something!Or nah? i will check?")
-some_sentence.value = "MyString has something!Or nah? i will check?"
+some_sentence = MyString(456)
+some_sentence.value = 456
+# some_sentence = MyString("MyString has something!Or nah? i will check?")
+# some_sentence.value = "MyString has something!Or nah? i will check?"
 print(some_sentence.is_question())
+print(some_sentence.count_sentences())
